434f25513129051088f4e2d2a95ad8f0ded539d0
Mike Wooten <mwooten@fortinet.com>
2022-09-03 07:47:12 -0500

    tomap syntax

--- aws_vpc/main.tf
+++ aws_vpc/main.tf
@@ -6,8 +6,8 @@ locals {
 resource "aws_vpc" "vpc" {
   cidr_block            = var.vpc_cidr
   enable_dns_hostnames  = true
-  tags = merge(local.id_tag,
-            tomap("Name", "${var.customer_prefix}-${var.environment}-${var.vpc_name}-vpc"),
-            tomap("Environment", var.environment))
+  tags = merge(tomap({Name = (var.customer_prefix)-(var.environment)-(var.vpc_name)-vpc}),
+            tomap({Environment = (var.environment)}),
+            local.id_tag)
 }
 
