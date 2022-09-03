45475af62302b62250416cc2dcb316f8557ba762
Mike Wooten <mwooten@fortinet.com>
2022-09-03 11:54:28 -0500

    moving Environment and ID Tag to provider block

--- aws_vpc/main.tf
+++ aws_vpc/main.tf
@@ -1,13 +1,9 @@
 
-locals {
-    id_tag = tomap({(var.vpc_tag_key) = (var.vpc_tag_value)})
-}
-
 resource "aws_vpc" "vpc" {
   cidr_block            = var.vpc_cidr
   enable_dns_hostnames  = true
-  tags = merge(tomap({Name = (var.customer_prefix)-(var.environment)-(var.vpc_name)}),
-            tomap({Environment = (var.environment)}),
-            local.id_tag)
+  tags = {
+    Name = var.vpc_name
+  }
 }
 
--- aws_vpc/variables.tf
+++ aws_vpc/variables.tf
@@ -1,28 +1,8 @@
 
-variable "customer_prefix" {
-  description = "Customer Prefix to apply to all resources"
-}
-
-variable "environment" {
-  description = "The Tag Environment in the S3 tag"
-}
-
 variable "vpc_name" {
   description = "The VPC Name"
 }
 
-variable "aws_region" {
-  description = "The AWS region to use"
-}
-
 variable "vpc_cidr" {
     description = "CIDR for the whole VPC"
-}
-variable "vpc_tag_key" {
-    description = "Random Tag Key to place on VPC for data ID"
-    default     = ""
-}
-variable "vpc_tag_value" {
-    description = "Random Tag Value to place on VPC for data ID"
-    default     = ""
 }
