b81ac9a1981b24bd0c66a826baf2733137fb0d3f
Mike Wooten <mwooten@fortinet.com>
2022-09-03 07:36:39 -0500

    deprecated map(), changed to tomap()

--- aws_vpc/main.tf
+++ aws_vpc/main.tf
@@ -1,13 +1,13 @@
 
 locals {
-    id_tag = map(var.vpc_tag_key, var.vpc_tag_value)
+    id_tag = tomap(var.vpc_tag_key, var.vpc_tag_value)
 }
 
 resource "aws_vpc" "vpc" {
   cidr_block            = var.vpc_cidr
   enable_dns_hostnames  = true
   tags = merge(local.id_tag,
-            map("Name", "${var.customer_prefix}-${var.environment}-${var.vpc_name}-vpc"),
-            map("Environment", var.environment))
+            tomap("Name", "${var.customer_prefix}-${var.environment}-${var.vpc_name}-vpc"),
+            tomap("Environment", var.environment))
 }
 
