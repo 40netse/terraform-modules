9c0f383f8ea48a062e44e7fb0c40e256bca69adc
Mike Wooten <mwooten@fortinet.com>
2022-09-07 16:33:57 -0500

    backout previous

--- aws_tgw_attachment/main.tf
+++ aws_tgw_attachment/main.tf
@@ -5,7 +5,7 @@ resource "aws_ec2_transit_gateway_vpc_attachment" "tgw-attach" {
   }
   transit_gateway_id                              = var.transit_gateway_id
   vpc_id                                          = var.vpc_id
-  subnet_ids                                      = [ var.subnet1, var.subnet2 ]
+  subnet_ids                                      = var.subnet_ids
   transit_gateway_default_route_table_association = var.transit_gateway_default_route_table_association
   transit_gateway_default_route_table_propagation = var.transit_gateway_default_route_table_propogation
   dns_support                                     = var.dns_support
