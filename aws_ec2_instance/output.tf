de9d15d3b101ed69ea871e1cf76e91c0883aff8a
Mike Wooten <mwooten@fortinet.com>
2023-04-12 08:28:50 -0500

    wrong order for preallocated IP

--- aws_ec2_instance/output.tf
+++ aws_ec2_instance/output.tf
@@ -16,5 +16,5 @@ output "network_private_interface_ip" {
   value = aws_network_interface.private_eni.*.private_ip
 }
 output "public_eip" {
-  value = var.use_preallocated_elastic_ip ? aws_eip.EIP.*.public_ip : aws_eip_association.AEIP.*.public_ip
+  value = var.use_preallocated_elastic_ip ? aws_eip_association.AEIP.*.public_ip : aws_eip.EIP.*.public_ip
 }
