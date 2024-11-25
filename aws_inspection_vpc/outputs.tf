c469427371782539177f09f69a9eed49b820518d
Mike Wooten <20910411+40netse@users.noreply.github.com>
2024-11-25 09:35:09 -0600

    update

--- aws_inspection_vpc/outputs.tf
+++ aws_inspection_vpc/outputs.tf
@@ -2,7 +2,3 @@ output "vpc_id" {
   value       = module.vpc.vpc_id
   description = "The VPC Id of the newly created VPC."
 }
-output "vpc_main_route_table_id" {
-  value       = module.vpc.main_route_table_id
-  description = "Main Route Table Associated with VPC"
-}
