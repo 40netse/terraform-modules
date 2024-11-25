1cbfd78d959da1d4afd18019e007dc4f75569b8c
Mike Wooten <20910411+40netse@users.noreply.github.com>
2024-11-25 09:33:49 -0600

    update

--- aws_inspection_vpc/outputs.tf
+++ aws_inspection_vpc/outputs.tf
@@ -1,8 +1,8 @@
 output "vpc_id" {
-  value       = aws_vpc.vpc.id
+  value       = module.vpc.vpc_id
   description = "The VPC Id of the newly created VPC."
 }
 output "vpc_main_route_table_id" {
-  value       = aws_vpc.vpc.main_route_table_id
+  value       = module.vpc.main_route_table_id
   description = "Main Route Table Associated with VPC"
 }
--- aws_inspection_vpc/variables.tf
+++ aws_inspection_vpc/variables.tf
@@ -1,24 +1,27 @@
 
 variable "vpc_name" {
   description = "The VPC Name"
+  type = string
 }
 variable "vpc_cidr" {
-    description = "CIDR for the whole VPC"
+  description = "CIDR for the whole VPC"
+  type = string
 }
 variable "subnet_bits" {
-    description = "Number of bits to use for each subnet"
+  description = "Number of bits to use for each subnet"
+  type = number
 }
 variable "availability_zone_1" {
-    description = "Availability Zone 1"
+  description = "Availability Zone 1"
 }
 variable "availability_zone_2" {
-    description = "Availability Zone 2"
+  description = "Availability Zone 2"
 }
 variable "enable_nat_gateway" {
   description = "Boolean to enable NAT Gateway to be created within the inspection vpc"
   type = bool
 }
 variable "named_tgw" {
-    description = "Name of the TGW to attach to"
-    type = string
+  description = "Name of the TGW to attach to"
+  type = string
 }
