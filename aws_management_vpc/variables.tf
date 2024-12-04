94065dca2a1523a622fdc5705b268a5849e1b14b
Mike Wooten <20910411+40netse@users.noreply.github.com>
2024-12-04 15:13:32 -0600

    first checkin

---
+++ aws_management_vpc/variables.tf
@@ -0,0 +1,107 @@
+variable "aws_region" {
+  description = "The AWS region to use"
+}
+variable "random_string" {
+  description = "The random string to append to any run specific resources"
+  type = string
+}
+variable "keypair" {
+  description = "Keypair for instances that support keypairs"
+}
+variable "acl" {
+  description = "The acl for linux instances"
+}
+variable "my_ip" {
+    description = "CIDR for my IP to restrict security group"
+}
+variable "vpc_cidr_ns_inspection" {
+    description = "CIDR for the whole NS inspection VPC"
+}
+variable "vpc_name" {
+  description = "The VPC Name"
+  type = string
+}
+variable "vpc_cidr" {
+  description = "CIDR for the whole VPC"
+  type = string
+}
+variable "subnet_bits" {
+  description = "Number of bits to use for each subnet"
+  type = number
+}
+variable "availability_zone_1" {
+  description = "Availability Zone 1"
+}
+variable "availability_zone_2" {
+  description = "Availability Zone 2"
+}
+variable "named_tgw" {
+  description = "Name of the TGW to attach to"
+  type = string
+}
+variable "enable_tgw_attachment" {
+  description = "Boolean to enable attachment to a named TGW"
+  type = bool
+}
+variable "enable_jump_box" {
+  description = "Boolean to allow creation of Linux Jump Box in Inspection VPC"
+  type        = bool
+}
+variable "enable_jump_box_public_ip" {
+  description = "Boolean to allow creation of Linux Jump Box public IP in Inspection VPC"
+  type        = bool
+}
+variable "enable_fortimanager" {
+  description = "Boolean to allow creation of FortiManager in Inspection VPC"
+  type        = bool
+}
+variable "fortimanager_license_file" {
+  description = "Full path for FortiManager License"
+  type        = string
+  default     = ""
+}
+variable "enable_linux_spoke_instances" {
+  description = "Boolean to allow creation of Linux Spoke Instances in East and West VPCs"
+  type        = bool
+}
+variable "enable_fortimanager_public_ip" {
+  description = "Boolean to allow creation of FortiManager public IP in Inspection VPC"
+  type        = bool
+}
+variable "fortimanager_instance_type" {
+  description = "Instance type for fortimanager"
+}
+variable "fortimanager_os_version" {
+  description = "Fortimanager OS Version for the AMI Search String"
+}
+variable "fortimanager_host_ip" {
+  description = "Fortimanager IP Address"
+}
+variable "enable_fortianalyzer" {
+  description = "Boolean to allow creation of FortiAnalyzer in Inspection VPC"
+  type        = bool
+}
+variable "fortianalyzer_license_file" {
+  description = "Full path for FortiAnalyzer License"
+  type        = string
+  default     = ""
+}
+variable "fortianalyzer_instance_type" {
+  description = "Instance type for fortianalyzer"
+}
+variable "fortianalyzer_os_version" {
+  description = "Fortianalyzer OS Version for the AMI Search String"
+}
+variable "fortianalyzer_host_ip" {
+  description = "Fortianalyzer IP Address"
+}
+variable "enable_fortianalyzer_public_ip" {
+  description = "Boolean to allow creation of FortiAnalyzer public IP in Inspection VPC"
+  type        = bool
+}
+variable "linux_instance_type" {
+  description = "Linux Endpoint Instance Type"
+}
+variable "linux_host_ip" {
+  description = "Fortigate Host IP for all subnets"
+}
---
+++ aws_management_vpc/vpc_management.tf
@@ -0,0 +1,194 @@
+locals {
+    public_subnet_index = 0
+}
+locals {
+  rfc1918_192 = "192.168.0.0/16"
+}
+locals {
+  rfc1918_10 = "10.0.0.0/8"
+}
+locals {
+  rfc1918_172 = "172.16.0.0/12"
+}
+locals {
+  management_public_subnet_cidr_az1 = cidrsubnet(var.vpc_cidr, var.subnet_bits, local.public_subnet_index)
+}
+locals {
+  management_public_subnet_cidr_az2 = cidrsubnet(var.vpc_cidr, var.subnet_bits, local.public_subnet_index + 1)
+}
+
+locals {
+  management_subnet_ids = [module.subnet-management-public-az1.id, module.subnet-management-public-az2.id]
+}
+
+data "aws_ec2_transit_gateway" "tgw" {
+  count = enable_tgw_attachment ? 1 : 0
+  filter {
+    name   = "tag:Name"
+    values = [var.named_tgw]
+  }
+  filter {
+    name   = "state"
+    values = ["available"]
+  }
+}
+
+#
+# Spoke VPC
+#
+module "vpc-management" {
+  source = "git::https://github.com/40netse/terraform-modules.git//aws_vpc"
+  vpc_name                   = "${var.vpc_name}-management-vpc"
+  vpc_cidr                   = var.vpc_cidr
+}
+module "vpc-igw-management" {
+  source = "git::https://github.com/40netse/terraform-modules.git//aws_igw"
+  igw_name                   = "${var.vpc_name}-management-igw"
+  vpc_id                     = module.vpc-management.vpc_id
+}
+module "vpc-transit-gateway-attachment-management" {
+  source                         = "git::https://github.com/40netse/terraform-modules.git//aws_tgw_attachment"
+  count                          = var.enable_tgw_attachment ? 1 : 0
+  tgw_attachment_name            = "${var.vpc_name}-mgmt-inspection-tgw-attachment"
+
+  transit_gateway_id                              = data.aws_ec2_transit_gateway.tgw.id
+  subnet_ids                                      = [module.subnet-management-public-az1.id, module.subnet-management-public-az2.id]
+  transit_gateway_default_route_table_propogation = "true"
+  appliance_mode_support                          = "enable"
+  vpc_id                                          = module.vpc-management.vpc_id
+}
+
+#
+# AZ 1
+#
+module "subnet-management-public-az1" {
+  source = "git::https://github.com/40netse/terraform-modules.git//aws_subnet"
+  subnet_name                = "${var.vpc_name}-management-public-az1-subnet"
+
+  vpc_id                     = module.vpc-management.vpc_id
+  availability_zone          = var.availability_zone_1
+  subnet_cidr                = local.management_public_subnet_cidr_az1
+}
+module "management-public-route-table-az1" {
+  source  = "git::https://github.com/40netse/terraform-modules.git//aws_route_table"
+  rt_name = "${var.vpc_name}-management-public-rt-az1"
+
+  vpc_id                     = module.vpc-management.vpc_id
+}
+
+module "management-public-route-table_association-az1" {
+  source   = "git::https://github.com/40netse/terraform-modules.git//aws_route_table_association"
+
+  subnet_ids                 = module.subnet-management-public-az1.id
+  route_table_id             = module.management-public-route-table-az1.id
+}
+#
+# AZ 2
+#
+module "subnet-management-public-az2" {
+  source = "git::https://github.com/40netse/terraform-modules.git//aws_subnet"
+  subnet_name                = "${var.vpc_name}-management-public-az2-subnet"
+
+  vpc_id                     = module.vpc-management.vpc_id
+  availability_zone          = var.availability_zone_2
+  subnet_cidr                = local.management_public_subnet_cidr_az2
+}
+module "management-public-route-table-az2" {
+  source  = "git::https://github.com/40netse/terraform-modules.git//aws_route_table"
+  rt_name = "${var.vpc_name}-management-public-rt-az2"
+
+  vpc_id                     = module.vpc-management.vpc_id
+}
+
+module "management-public-route-table_association-az2" {
+  source   = "git::https://github.com/40netse/terraform-modules.git//aws_route_table_association"
+
+  subnet_ids                 = module.subnet-management-public-az2.id
+  route_table_id             = module.management-public-route-table-az2.id
+}
+#
+# Default route table that is created with the main VPC.
+#
+resource "aws_default_route_table" "route_management" {
+  default_route_table_id = module.vpc-management.vpc_main_route_table_id
+  tags = {
+    Name = "default table for vpc management (unused)"
+  }
+}
+resource "aws_security_group" "management-vpc-sg" {
+  description = "Security Group for ENI in the management VPC"
+  vpc_id = module.vpc-management.vpc_id
+  ingress {
+    description = "Allow egress ALL"
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = [ "0.0.0.0/0" ]
+  }
+  egress {
+    description = "Allow egress ALL"
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = [ "0.0.0.0/0" ]
+  }
+}
+#
+# Routes for the route table.
+#
+resource "aws_route" "management-mgmt-public-default-route-ngw-az1" {
+  depends_on             = [module.vpc-igw-management]
+  route_table_id         = module.management-public-route-table-az1.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = module.vpc-igw-management.igw_id
+}
+
+resource "aws_route" "inspection-mgmt-gwlbe-192-route-igw-az1" {
+  count                  = var.enable_tgw_attachment ? 1 : 0
+  depends_on             = [module.vpc-transit-gateway-attachment-management]
+  route_table_id         = module.management-public-route-table-az1.id
+  destination_cidr_block = local.rfc1918_192
+  transit_gateway_id     = data.aws_ec2_transit_gateway.tgw.id
+}
+resource "aws_route" "inspection-mgmt-gwlbe-10-route-igw-az1" {
+  count                  = var.enable_tgw_attachment ? 1 : 0
+  depends_on             = [module.vpc-transit-gateway-attachment-management]
+  route_table_id         = module.management-public-route-table-az1.id
+  destination_cidr_block = local.rfc1918_10
+  transit_gateway_id     = data.aws_ec2_transit_gateway.tgw.id
+}
+resource "aws_route" "inspection-mgmt-gwlbe-172-route-igw-az1" {
+  count                  = var.enable_tgw_attachment ? 1 : 0
+  depends_on             = [module.vpc-transit-gateway-attachment-management]
+  route_table_id         = module.management-public-route-table-az1.id
+  destination_cidr_block = local.rfc1918_172
+  transit_gateway_id     = data.aws_ec2_transit_gateway.tgw.id
+}
+resource "aws_route" "management-mgmt-public-default-route-ngw-az2" {
+  depends_on             = [module.vpc-igw-management]
+  route_table_id         = module.management-public-route-table-az2.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = module.vpc-igw-management.igw_id
+}
+
+resource "aws_route" "inspection-mgmt-gwlbe-192-route-igw-az2" {
+  count                  = var.enable_tgw_attachment ? 1 : 0
+  depends_on             = [module.vpc-transit-gateway-attachment-management]
+  route_table_id         = module.management-public-route-table-az2.id
+  destination_cidr_block = local.rfc1918_192
+  transit_gateway_id     = data.aws_ec2_transit_gateway.tgw.id
+}
+resource "aws_route" "inspection-mgmt-gwlbe-10-route-igw-az2" {
+  count                  = var.enable_tgw_attachment ? 1 : 0
+  depends_on             = [module.vpc-transit-gateway-attachment-management]
+  route_table_id         = module.management-public-route-table-az2.id
+  destination_cidr_block = local.rfc1918_10
+  transit_gateway_id     = data.aws_ec2_transit_gateway.tgw.id
+}
+resource "aws_route" "inspection-mgmt-gwlbe-172-route-igw-az2" {
+  count                  = var.enable_tgw_attachment ? 1 : 0
+  depends_on             = [module.vpc-transit-gateway-attachment-management]
+  route_table_id         = module.management-public-route-table-az2.id
+  destination_cidr_block = local.rfc1918_172
+  transit_gateway_id     = data.aws_ec2_transit_gateway.tgw.id
+}
