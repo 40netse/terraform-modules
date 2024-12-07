6f2d2d7741d4483c07faa9c176c4d5b13bfe3e0a
Mike Wooten <20910411+40netse@users.noreply.github.com>
2024-12-07 12:33:49 -0600

    update

--- aws_management_vpc/outputs.tf
+++ aws_management_vpc/outputs.tf
@@ -10,3 +10,7 @@ output "management_tgw_attachment_id" {
   value       = module.vpc-transit-gateway-attachment-management[0].tgw_attachment_id
   description = "The transit gateway attachment id for the management VPC."
 }
+output "management_tgw_route_table_id" {
+  value       = aws_ec2_transit_gateway_route_table.management[0].id
+  description = "The transit gateway route table id for the management VPC."
+}
