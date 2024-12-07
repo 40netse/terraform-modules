51363fc1bc943ec206f7b8b87045a607016f51e1
Mike Wooten <20910411+40netse@users.noreply.github.com>
2024-12-07 09:06:56 -0600

    update

--- aws_management_vpc/outputs.tf
+++ aws_management_vpc/outputs.tf
@@ -6,3 +6,7 @@ output "igw_id" {
   value       = module.vpc-igw-management.igw_id
   description = "The IGW Id of the newly created IGW."
 }
+output "management_tgw_attachment_id" {
+  value       = module.vpc-transit-gateway-attachment-management.id
+  description = "The transit gateway attachment id for the management VPC."
+}
