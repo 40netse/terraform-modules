50be004934ae1f0b487ed935e430afd320a1d96a
Mike Wooten <20910411+40netse@users.noreply.github.com>
2024-12-09 08:59:59 -0600

    update

--- aws_management_vpc/outputs.tf
+++ aws_management_vpc/outputs.tf
@@ -7,34 +7,34 @@ output "igw_id" {
   description = "The IGW Id of the newly created IGW."
 }
 output "management_tgw_attachment_id" {
-  value       = module.vpc-transit-gateway-attachment-management[0].tgw_attachment_id
+  value = var.enable_tgw_attachment ? module.vpc-transit-gateway-attachment-management[0].tgw_attachment_id :null
   description = "The transit gateway attachment id for the management VPC."
 }
 output "management_tgw_route_table_id" {
-  value       = aws_ec2_transit_gateway_route_table.management[0].id
+  value = var.enable_tgw_attachment ? aws_ec2_transit_gateway_route_table.management[0].id : null
   description = "The transit gateway route table id for the management VPC."
 }
 output "jump_box_public_ip" {
-  value       = module.inspection_instance_jump_box[0].public_eip
+  value = var.enable_jump_box_public_ip ? module.inspection_instance_jump_box[0].public_eip : null
   description = "The public IP address of the jump box."
 }
 output "jump_box_private_ip" {
-  value       = module.inspection_instance_jump_box[0].network_public_interface_ip
+  value = var.enable_jump_box ? module.inspection_instance_jump_box[0].network_public_interface_ip : null
   description = "The private IP address of the jump box."
 }
 output "fortimanager_public_ip" {
-  value       = module.fortimanager[0].public_eip
+  value = var.enable_fortimanager_public_ip ? module.fortimanager[0].public_eip : null
   description = "The public IP address of the FortiManager."
 }
 output "fortimanager_private_ip" {
-  value       = module.fortimanager[0].network_public_interface_ip
+  value = var.enable_fortimanager ? module.fortimanager[0].network_public_interface_ip : null
   description = "The private IP address of the FortiManager."
 }
 output "fortianalyzer_public_ip" {
-  value       = module.fortianalyzer[0].public_eip
+  value = var.enable_fortianalyzer_public_ip ? module.fortianalyzer[0].public_eip : null
   description = "The public IP address of the fortianalyzer."
 }
 output "fortianalyzer_private_ip" {
-  value       = module.fortianalyzer[0].network_public_interface_ip
+  value = var.enable_fortianalyzer ? module.fortianalyzer[0].network_public_interface_ip : null
   description = "The private IP address of the fortianalyzer."
 }
