fab150230915f7fdcdb9800653a08ab6ec67c805
Mike Wooten <20910411+40netse@users.noreply.github.com>
2025-06-02 13:35:31 -0500

    added updated ubuntu ami search string

--- aws_management_vpc/vpc_management.tf
+++ aws_management_vpc/vpc_management.tf
@@ -46,7 +46,7 @@ resource aws_security_group "fortimanager_sg" {
     from_port = 443
     to_port = 443
     protocol = "tcp"
-    cidr_blocks = [ var.my_ip ]
+    cidr_blocks = [ var.my_ip, "10.3.0.0/16" ]
   }
   ingress {
     description = "Allow HTTP from Anywhere IPv4 (change this to My IP)"
