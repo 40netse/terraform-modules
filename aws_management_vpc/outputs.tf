39d3180b36485b26c530894d87ff334ca22dc743
Mike Wooten <20910411+40netse@users.noreply.github.com>
2025-11-30 08:17:48 -0600

    error in the outputs when fmgr or faz isn't enabled

--- aws_management_vpc/outputs.tf
+++ aws_management_vpc/outputs.tf
@@ -43,7 +43,7 @@ output "jump_box_instance_id" {
   description = "The instance ID of the jump box."
 }
 output "fortimanager_public_ip" {
-  value       = var.enable_fortimanager_public_ip ? module.fortimanager[0].public_eip : null
+  value       = var.enable_fortimanager &&  var.enable_fortimanager_public_ip ? module.fortimanager[0].public_eip : null
   description = "The public IP address of the FortiManager."
 }
 output "fortimanager_private_ip" {
@@ -51,7 +51,7 @@ output "fortimanager_private_ip" {
   description = "The private IP address of the FortiManager."
 }
 output "fortianalyzer_public_ip" {
-  value       = var.enable_fortianalyzer_public_ip ? module.fortianalyzer[0].public_eip : null
+  value       = var.enable_fortianalyzer && var.enable_fortianalyzer_public_ip ? module.fortianalyzer[0].public_eip : null
   description = "The public IP address of the fortianalyzer."
 }
 output "fortianalyzer_private_ip" {
